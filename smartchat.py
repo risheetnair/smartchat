f398ea7db2edc36c4b74c2320f00f99276ff4539
risheetnair <risheetnair@outlook.com>
2022-03-01 22:50:41 -0500

    imported necessary libraries

---
+++ smartchat.py
@@ -0,0 +1,9 @@
+from newspaper import Article
+import random
+import string
+import nltk
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
