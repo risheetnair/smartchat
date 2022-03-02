7fc8865034ab4ee80127189e9297dc1581e1283f
risheetnair <risheetnair@outlook.com>
2022-03-01 22:56:24 -0500

    provided article link to scrape and applied nlp to it

--- smartchat.py
+++ smartchat.py
@@ -7,3 +7,11 @@ from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import warnings
 warnings.filterwarnings('ignore')
+
+nltk.download('punkt', quiet=True)
+
+article = Article('https://www.mayoclinic.org/diseases-conditions/chronic-kidney-disease/symptoms-causes/syc-20354521')
+article.download()
+article.parse()
+article.nlp()
+corpus = article.text
